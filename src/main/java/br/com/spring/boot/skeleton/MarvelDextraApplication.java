1e055d83dd2dfcad980a46dbe7d4e6b3fb2b2917
Diego de Santana <diegosant.per@gmail.com>
2020-09-10 21:26:21 -0300

    Add a initial setup to project be able to work on Tomcat, to run in Docker and be tested with JUnit.

---
+++ src/main/java/br/com/spring/boot/skeleton/MarvelDextraApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.boot.skeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelDextraApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MarvelDextraApplication.class, args);
+  }
+
+}
