d4c9a75a15e1910a8760a17326b8adc83569f1fd
disantana <diegosant.per@gmail.com>
2020-09-13 22:56:25 -0300

    Add new test case to EventRepository and StoryRepository.

---
+++ couchbase_repository/src/test/java/com/marveldextra/couchbase_repository/repository/EventRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.marveldextra.couchbase_repository.repository;
+
+import com.marveldextra.couchbase_repository.entity.Character;
+import com.marveldextra.couchbase_repository.entity.Event;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class EventRepositoryTest extends Assertions {
+
+    @Autowired private EventRepository repository;
+    @Autowired private CharacterRepository characterRepository;
+
+    @Test
+    public void shouldReturnEventSearchedByCharacter(){
+        Character character = characterRepository.save(Character.builder()
+                .name("Comic's character Teste")
+                .description("Character's description")
+                .build());
+
+        Event event = repository.save(Event.builder()
+                .character(character)
+                .description("Event description")
+                .title("Event title")
+                .build());
+        List<Event> events= repository.findAllByCharacter(character.getName());
+        assertFalse(events.isEmpty());
+        assertTrue(events.get(0).getDescription().equals("Event description"));
+    }
+}
