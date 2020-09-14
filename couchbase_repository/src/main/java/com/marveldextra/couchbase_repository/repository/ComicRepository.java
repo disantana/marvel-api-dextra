93c0a692501359ac2c5b1a2f3ffeae11738261a2
disantana <diegosant.per@gmail.com>
2020-09-13 23:17:59 -0300

    Fix : change parameter's to name in findAllByCharacter method.

--- couchbase_repository/src/main/java/com/marveldextra/couchbase_repository/repository/ComicRepository.java
+++ couchbase_repository/src/main/java/com/marveldextra/couchbase_repository/repository/ComicRepository.java
@@ -1,12 +1,8 @@
 package com.marveldextra.couchbase_repository.repository;
 
-import com.marveldextra.couchbase_repository.entity.Character;
 import com.marveldextra.couchbase_repository.entity.Comic;
-import org.springframework.data.couchbase.core.query.N1QLExpression;
-import org.springframework.data.couchbase.core.query.N1QLQuery;
 import org.springframework.data.couchbase.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -14,5 +10,5 @@ import java.util.List;
 public interface ComicRepository extends CrudRepository<Comic,String> {
    
     @Query("#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND character.name = $1")
-    List<Comic> findAllByCharacter(String characterName);
+    List<Comic> findAllByCharacter(String name);
 }
