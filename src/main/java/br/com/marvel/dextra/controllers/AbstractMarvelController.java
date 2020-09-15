528e659cac64e0c688416e717a63d0bb24775149
Diego de Santana <diegosant.per@gmail.com>
2020-09-15 05:30:12 -0300

    Add LINK_TO attributes.

--- src/main/java/br/com/marvel/dextra/controllers/AbstractMarvelController.java
+++ src/main/java/br/com/marvel/dextra/controllers/AbstractMarvelController.java
@@ -4,4 +4,9 @@ abstract class AbstractMarvelController {
   protected static final int PAGE_SIZE = 30;
   protected static final int FIRST_PAGE = 0;
   protected static final String BASE_PATH = "/v1/public/characters";
+  protected static final String LINK_TO_CHARACTER = "characters";
+  protected static final String LINK_TO_EVENT = "events";
+  protected static final String LINK_TO_STORY = "stories";
+  protected static final String LINK_TO_COMIC = "comics";
+  protected static final String LINK_TO_SERIE = "series";
 }
