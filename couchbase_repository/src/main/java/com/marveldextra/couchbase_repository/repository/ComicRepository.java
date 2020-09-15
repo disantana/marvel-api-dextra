8c160803b547f7a66ce36ddb6f8eaa08647735a2
Diego de Santana <diegosant.per@gmail.com>
2020-09-15 01:18:26 -0300

    Add ComicController and create new method in ComicRepository to find all Comic.

--- couchbase_repository/src/main/java/com/marveldextra/couchbase_repository/repository/ComicRepository.java
+++ couchbase_repository/src/main/java/com/marveldextra/couchbase_repository/repository/ComicRepository.java
@@ -2,6 +2,8 @@ package com.marveldextra.couchbase_repository.repository;
 
 import com.marveldextra.couchbase_repository.entity.Comic;
 import org.springframework.data.couchbase.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -11,4 +13,6 @@ public interface ComicRepository extends CrudRepository<Comic,String> {
    
     @Query("#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND character.name = $1")
     List<Comic> findAllByCharacter(String name);
+
+    Page<Comic> findAll(Pageable pageable);
 }
---
+++ src/main/java/br/com/spring/boot/skeleton/controllers/ComicController.java
@@ -0,0 +1,35 @@
+package br.com.spring.boot.skeleton.controllers;
+
+import com.marveldextra.couchbase_repository.entity.Comic;
+import com.marveldextra.couchbase_repository.repository.ComicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(AbstractMarvelController.BASE_PATH)
+public class ComicController extends AbstractMarvelController{
+
+  @Autowired
+  ComicRepository repository;
+
+  @GetMapping("/{characterId}")
+  CollectionModel<EntityModel<Comic>> all(@PathVariable String characterId) {
+    List<EntityModel<Comic>> comics = repository.findAll(PageRequest.of(FIRST_PAGE,PAGE_SIZE))
+        .map(comic -> EntityModel.of(comic,
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CharacterController.class).getById(comic.getId())).withRel("comics")))
+        .stream().collect(Collectors.toList());
+
+    return CollectionModel.of(comics, WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CharacterController.class).all()).withSelfRel());
+
+  }
+}
