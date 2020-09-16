d0c4291db194d0550d1ed731bd20487b73593b22
Diego de Santana <diegosant.per@gmail.com>
2020-09-15 23:21:02 -0300

    Revert "Add Dockerfile to couchbase and docker-compose to startup project in local environment"
    This reverts commit 1d1f5cf7

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
 # marvel-api-dextra
 It's an API project to consume Marvel's API.
-
-### Orientações de instalação
-
-### Maven
-`mvn install -Dmaven.test.skip=true`
-### Docker:
-
-#### Criar imagem couchbase
-
-Execute o seguinte comando ao acessar o diretório do projeto: 
-`docker build -t dextra-api .`
-`cd src/main/resources/config/couchbase/`
-`docker build -t couchbase-custom .`
-
-`docker-compose run -d --service-ports --name couchbase couchbase`
-`docker-compose run -d --service-ports --name spring-boot spring-boot`
-
--- couchbase_repository/src/main/java/com/marveldextra/couchbase_repository/config/CouchbaseConfig.java
+++ couchbase_repository/src/main/java/com/marveldextra/couchbase_repository/config/CouchbaseConfig.java
@@ -1,43 +1,31 @@
 package com.marveldextra.couchbase_repository.config;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 
 @Configuration
 public class CouchbaseConfig extends AbstractCouchbaseConfiguration{
 
-  @Value("${couchbase_host}")
-  private String hostname;
+    @Override
+    public String getConnectionString() {
+        return "couchbase://172.17.0.2";
+    }
 
-  @Value("${couchbase_bucket}")
-  private String bucket;
+    @Override
+    public String getUserName() {
+        return "marvelapp";
+    }
 
-  @Value("${couchbase_bucket_password}")
-  private String password;
+    @Override
+    public String getPassword() {
+        return "marvel";
+    }
 
-  @Override
-  public String getConnectionString() {
-      return "couchbase://".concat(hostname);
-  }
-
-  @Override
-  public String getUserName() {
-      return bucket;
-  }
-
-  @Override
-  public String getPassword() {
-      return password;
-  }
-
-  @Override
-  public String getBucketName() {
-      return bucket;
-  }
-
-
-
+    @Override
+    public String getBucketName() {
+        return "marvel";
+    } 
+    
     
 }
