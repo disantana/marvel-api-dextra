aef17e0939691857c041d5e26e4d9842bc1b8e05
Diego de Santana <diegosant.per@gmail.com>
2020-09-16 14:07:07 -0300

    Add new exception type (MarvelGenericException) and update ControllerExceptionAdvice

--- src/main/java/br/com/marvel/dextra/controllers/ControllerExceptionAdvice.java
+++ src/main/java/br/com/marvel/dextra/controllers/ControllerExceptionAdvice.java
@@ -1,6 +1,7 @@
 package br.com.marvel.dextra.controllers;
 
 import br.com.marvel.dextra.exceptions.CharacterNotFoundException;
+import br.com.marvel.dextra.exceptions.MarvelGenericException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,13 @@ public class ControllerExceptionAdvice {
   @ExceptionHandler(CharacterNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   String employeeNotFoundHandler(CharacterNotFoundException ex) {
+    return ex.getMessage();
+  }
+
+  @ResponseBody
+  @ExceptionHandler(MarvelGenericException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  String genericError(MarvelGenericException ex) {
     return ex.getMessage();
   }
 }
